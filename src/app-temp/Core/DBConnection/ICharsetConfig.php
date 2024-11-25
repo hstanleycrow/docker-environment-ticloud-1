d0821902e33b60b0612912290cbd44c779c73411
Harold Crow <hstanleycrow@gmail.com>
2024-11-25 16:05:57 -0600

    Eliminar carpeta app-temp del proyecto

--- src/app-temp/Core/DBConnection/ICharsetConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Core\DBConnection;
-
-interface ICharsetConfig
-{
-    public function getCharset(): string;
-}
--- src/app-temp/Core/ImageUploader/ImageUploaderInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Core\ImageUploader;
-
-interface ImageUploaderInterface
-{
-    public function upload(): string;
-    public function isUploading(): bool;
-}
